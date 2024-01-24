69401eb168235636a4b50b00e04c5a8e288f9d9c
Oshadha Nipun <157488657+oshadha2k01@users.noreply.github.com>
2024-01-24 22:16:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
+// script.js
+
+function showLoginBox() {
+    document.getElementById("loginBox").style.display = "flex";
+}
+
+function hideLoginBox() {
+    document.getElementById("loginBox").style.display = "none";
+}
+
+function showOrderNowPopup() {
+    document.getElementById("orderNowPopup").style.display = "flex";
+}
+
+function hideOrderNowPopup() {
+    document.getElementById("orderNowPopup").style.display = "none";
+}
+
+function showBookPopup() {
+    document.getElementById("bookNowPopup").style.display = "flex";
+}
+
+function hideBookPopup() {
+    document.getElementById("bookNowPopup").style.display = "none";
+}
+
+function showOffersPopup() {
+    document.getElementById("offersPopup").style.display = "flex";
+}
+
+function hideOffersPopup() {
+    document.getElementById("offersPopup").style.display = "none";
+}
+
+function showAboutPopup() {
+    document.getElementById("aboutPopup").style.display = "flex";
+}
+
+function hideAboutPopup() {
+    document.getElementById("aboutPopup").style.display = "none";
+}
+
+function showContactPopup() {
+    document.getElementById("contactPopup").style.display = "flex";
+}
+
+function hideContactPopup() {
+    document.getElementById("contactPopup").style.display = "none";
+}
 
